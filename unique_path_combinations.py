a6baa1b33c5636031a2bf24d29e55a812bffb3ea
is4761 <ishanpshah001@gmail.com>
2025-07-17 00:20:56 -0400

    cool math trick

--- unique_path_combinations.py
+++ unique_path_combinations.py
@@ -1,10 +1,22 @@
-import math
+dict = {}
+
+def fact(a):
+    if a == 1:
+        return 1
+    if a in dict:
+        return dict[a]
+    dict[a] = a*fact(a-1)
+    return dict[a]
+
+def comb(n, k):
+    return fact(n)//(fact(k)*fact(n-k))
 
 def uniquePaths(m, n):
     path_len = m+n-2
     right_choice = m-1
     # down_choice = n-1
-    return math.comb(path_len, right_choice)
+    return comb(path_len, right_choice)
+        
 
 print(uniquePaths(7,3))
 print(uniquePaths(3, 7))
