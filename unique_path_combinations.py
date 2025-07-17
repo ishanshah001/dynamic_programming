5224dc7221d405bdc1e39865552ccfe64ed275d1
is4761 <ishanpshah001@gmail.com>
2025-07-16 23:42:31 -0400

    cool math trick

---
+++ unique_path_combinations.py
@@ -0,0 +1,10 @@
+import math
+
+def uniquePaths(m, n):
+    path_len = m+n-2
+    right_choice = m-1
+    # down_choice = n-1
+    return math.comb(path_len, right_choice)
+
+print(uniquePaths(7,3))
+print(uniquePaths(3, 7))
